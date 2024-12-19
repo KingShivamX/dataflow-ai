3c594f6ce20ab89bf3ff075309d467562f54241e
KingShivamX <shivamhippalgave@gmail.com>
2024-12-20 01:25:50 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import Home from "./components/Home"
-import DatasetSelection from "./components/DatasetSelection"
-import AlgorithmImplementation from "./components/AlgorithmImplementation"
-import Visualization from "./components/Visualization"
 import Header from "./components/common/Header"
 import Footer from "./components/common/Footer"
 
@@ -13,15 +10,6 @@ const App = () => {
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route
-                    path="/dataset-selection"
-                    element={<DatasetSelection />}
-                />
-                <Route
-                    path="/algorithm-implementation"
-                    element={<AlgorithmImplementation />}
-                />
-                <Route path="/visualization" element={<Visualization />} />
             </Routes>
             <Footer />
         </>
--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,13 +2,10 @@ import React from "react"
 
 const Footer = () => {
     return (
-        <footer className="bg-blue-600 p-4">
-            <div className="container mx-auto flex flex-col md:flex-row justify-center items-center">
-                <p className="text-white text-center md:text-left mb-2 md:mb-0">
-                    &copy; {new Date().getFullYear()} DataFlowAI. All rights
-                    reserved.
-                </p>
-            </div>
+        <footer className="bg-yellow-400 p-4 flex justify-start items-center min-h-[10vh]">
+            <p className="text-lg">
+                &copy; 2024 DataFlowAI. All rights reserved.
+            </p>
         </footer>
     )
 }
